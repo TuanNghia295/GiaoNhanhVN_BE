ae5790618f228c07f2f20f08036cfb82c0f1205e
vietvd <vietvodinh12547@gmail.com>
2025-06-16 13:10:30 +0000

    fix: update

--- src/instrument.ts
+++ src/instrument.ts
@@ -2,7 +2,7 @@
 import * as Sentry from '@sentry/nestjs';
 
 Sentry.init({
-  dsn: 'https://2f1443653299b67660b01c0b3c391e9e@o4508644943331328.ingest.de.sentry.io/4508644945100880',
+  dsn: 'https://6bde863406e99de385b3498a25f6ea3b@o4508644943331328.ingest.de.sentry.io/4509508519460944',
 
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
