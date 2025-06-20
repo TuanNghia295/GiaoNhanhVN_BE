0a0ec5c9a0a311eadc6b2968b1e73b7c0f7ba5b3
vietvd <vietvodinh12547@gmail.com>
2025-06-20 03:09:04 +0000

    feat: update

--- drizzle/0001_brief_phil_sheldon.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "delivers" ALTER COLUMN "rating" SET DATA TYPE numeric(15, 2);--> statement-breakpoint
-ALTER TABLE "delivers" ALTER COLUMN "point" SET DATA TYPE numeric(15, 2);--> statement-breakpoint
-ALTER TABLE "settings" ALTER COLUMN "start_night_time" SET DEFAULT '2025-06-17 16:30:16.418';--> statement-breakpoint
-ALTER TABLE "settings" ALTER COLUMN "end_night_time" SET DEFAULT '2025-06-17 16:30:16.418';
