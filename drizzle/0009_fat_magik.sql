82f4777bcd9acabb15612bbdea89032e468043b3
vietvd <vietvodinh12547@gmail.com>
2025-07-24 06:44:30 +0000

    fix: xóa các field thừa không dùng

---
+++ drizzle/0009_fat_magik.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "voucher_usages" DROP COLUMN "id";--> statement-breakpoint
+ALTER TABLE "voucher_usages" DROP COLUMN "updated_at";--> statement-breakpoint
+ALTER TABLE "voucher_usages" DROP COLUMN "created_at";--> statement-breakpoint
+ALTER TABLE "vouchers" DROP COLUMN "used_count";
