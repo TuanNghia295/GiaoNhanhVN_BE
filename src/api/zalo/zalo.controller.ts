753ad495b519c90622d203d43af7591693145911
vietvd <vietvodinh12547@gmail.com>
2025-08-19 01:55:20 +0000

    feat: add Zalo callback endpoint and implement token retrieval logic

--- src/api/zalo/zalo.controller.ts
+++ src/api/zalo/zalo.controller.ts
@@ -1,8 +1,10 @@
 import { LoginResDto } from '@/api/auth/dto/login.res.dto';
 import { OTPCodeResDto } from '@/api/zalo/dto/otp-code.res.dto';
 import { VerifyOtpReqDto } from '@/api/zalo/dto/verify-otp.req.dto';
+import { ZaloCallbackReqDto } from '@/api/zalo/dto/zalo-callback.req.dto';
 import { ApiPublic } from '@/decorators/http.decorators';
 import { Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { ZaloService } from './zalo.service';
 
 @Controller('zalo')
@@ -16,6 +18,15 @@ export class ZaloController {
   @Get('send-otp')
   async sendOtpToPhone(@Query('phone') phone: string) {
     return await this.zaloService.sendZaloOtp(phone);
+  }
+
+  @ApiExcludeEndpoint()
+  @ApiPublic({
+    summary: 'Callback từ Zalo',
+  })
+  @Get('callback')
+  async callbackFromZalo(@Query() dto: ZaloCallbackReqDto) {
+    return await this.zaloService.callbackFromZalo(dto);
   }
 
   @ApiPublic({
